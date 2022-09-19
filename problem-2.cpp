612b310d3ca5e28f6a9f230941d37cbc06bcc078
Mohammad Omar <omar24md@gmail.com>
2022-09-19 20:47:49 +0600

    problem - 2 solved

--- problem-2.cpp
+++ problem-2.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
-
 int main(){
     int T;
     cout<<"How many number do you want to check? "<<endl;
     cin>> T;
     int i = 1;
     while(i <= T){
-        int num;
-        cout<<"Enter a Number: ";
-        cin>> num;
-        if(num % 2 == 0) cout<<"even"<<endl;
+        string inputNum;
+        cout<<"Enter a inputNumber: ";
+        cin>> inputNum;
+        int lastDig = inputNum[inputNum.length() - 1];
+        if(lastDig % 2 == 0) cout<<"even"<<endl;
         else cout<<"odd"<<endl;
     i++;
     }
